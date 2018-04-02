3ad1a0b7a14733e4d034efe430208f8d17e2b09d
bomjic <mikhail.baikov@gmail.com>
2018-04-03 02:01:45 +0300

    init

---
+++ Problem012/main.py
@@ -0,0 +1,16 @@
+
+
+if __name__ == "__main__":
+
+    triang_num_counter = 1
+    while triang_num_counter <= 7:
+        counter = 1
+        triang_num = 0
+        while counter <= triang_num_counter:
+            triang_num = triang_num + counter
+            counter += 1
+
+
+
+        print triang_num
+        triang_num_counter += 1

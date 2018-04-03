d09cac1020036260baba15a9a6e8864105742580
bomjic <mikhail.baikov@gmail.com>
2018-04-03 04:13:36 +0300

    update to add import

---
+++ Problem003/Largest_prime_factor.py
@@ -0,0 +1,20 @@
+import time
+#from ams import load_db
+
+def calc_primefactor(primefactor):
+    primefactors = []
+
+    while primefactor != 1:
+        divisor = 2
+        while divisor < primefactor + 1:
+            if primefactor % divisor == 0:
+                primefactors.append(divisor)
+                #print(divisor)
+                primefactor /= divisor
+                break
+            divisor += 1
+    return primefactors
+
+if __name__ == "__main__":
+    #calc_primefactor(28)
+    print calc_primefactor(600851475143)

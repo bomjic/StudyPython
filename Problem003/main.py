888261ca8aef2c31f0a14ad9c25d01304bff70c5
bomjic <mikhail.baikov@gmail.com>
2018-04-03 04:04:39 +0300

    update to add import

--- Problem003/main.py
+++ Problem003/main.py
@@ -1,13 +1,20 @@
 import time
+#from ams import load_db
 
-primefactor = 600851475143
-primefactors = []
+def calc_primefactor(primefactor):
+    primefactors = []
 
-while primefactor != 1:
-    divisor = 2
-    while divisor < primefactor + 1:
-        if primefactor % divisor == 0:
-            print(divisor)
-            primefactor /= divisor
-            break
-        divisor += 1
+    while primefactor != 1:
+        divisor = 2
+        while divisor < primefactor + 1:
+            if primefactor % divisor == 0:
+                primefactors.append(divisor)
+                print(divisor)
+                primefactor /= divisor
+                break
+            divisor += 1
+    return primefactors
+
+if __name__ == "__main__":
+    #calc_primefactor(28)
+    calc_primefactor(600851475143)

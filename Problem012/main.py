fc880c83631168649c1aa07c12c2a19408a95b17
bomjic <mikhail.baikov@gmail.com>
2018-04-03 04:34:09 +0300

    finish solving

--- Problem012/main.py
+++ Problem012/main.py
@@ -1,16 +1,39 @@
+import sys
+
 
 
 if __name__ == "__main__":
-
+    sys.path.append("/home/mikhail/Documents/StudyPython/Problem003")
+    from Largest_prime_factor import calc_primefactor
     triang_num_counter = 1
-    while triang_num_counter <= 7:
+    factors_amount = 0
+
+    #while triang_num_counter < 10:
+    while factors_amount < 500:
         counter = 1
         triang_num = 0
-        while counter <= triang_num_counter:
-            triang_num = triang_num + counter
+
+        # finding triangular numbers
+        for counter in range(1, triang_num_counter+1):
+            triang_num += counter
             counter += 1
 
+        #for factor in range(1, triang_num//2+1):
+        #    if (triang_num % factor == 0):
+        #        factors.append(factor)
 
 
-        print triang_num
+        prime_factors = []
+        prime_factors = calc_primefactor(triang_num)
+        unique_prime_factors = set(prime_factors)
+
+        factors_amount = 1
+        for factor in unique_prime_factors:
+            factors_amount *= prime_factors.count(factor)+1
+
+        #print triang_num
+        #print factors_amount
         triang_num_counter += 1
+
+    print triang_num
+    print factors_amount
